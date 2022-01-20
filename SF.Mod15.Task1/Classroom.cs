85cc4dc7a80d2a78d71e853ccdc07c418ec6f8b5
Евгений Гилев <euggil@yandex.ru>
2022-01-20 18:49:23 +0500

    Добавьте файлы проекта.

---
+++ SF.Mod15.Task1/Classroom.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SF.Mod15.Task1
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ SF.Mod15.Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SF.Mod15.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+                new Classroom {Students = {"Evgeniy", "Sergey", "Andrew"},},
+                new Classroom {Students = {"Anna", "Viktor", "Vladimir"},},
+                new Classroom {Students = {"Bulat", "Alex", "Galina"},}
+            };
+            var allStudents = GetAllStudents(classes);
+
+                      Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        /// <summary>
+        /// Собираем всех студентов в одну коллекцию
+        /// </summary>
+        /// <param name="classes">The classes.</param>
+        /// <returns>An array of string.</returns>
+        public static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStud =
+                from classroom in classes
+                from stud in classroom.Students
+                select stud;
+
+            return allStud.ToArray();
+        }
+    }
+}
